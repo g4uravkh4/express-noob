e40b73d7d734578be6c67093f56c6bbc776b2e8b
aura-gv <gauravkhatiwada4@gmail.com>
2023-07-24 22:03:41 +0545

    gg

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+// to use ejs as view engine
+app.set('view engine', 'ejs')
+
+// can use methods like get, post, delete, patch etc 
+app.get("/",(req, res) => {
+
+// res.send("not hello world")
+// res.status(500).json({ message: "Errrr"})
+// res.status(500).send("hi")
+// res.download('server.js')
+
+res.render("index", {text:"-hello"})
+
+})
+
+app.listen(3000)
